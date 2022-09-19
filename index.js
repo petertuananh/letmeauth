acca3e8f814ee66445607ed6ac3e7414f53bfbd1
petertadev <petertuananh@hotmail.com>
2022-09-19 18:54:20 +0700

    Fixed some bugs

--- index.js
+++ index.js
@@ -2,23 +2,42 @@ const axios = require("axios");
 const rootURL = 'https://letmeauth.xyz'
 
 exports.checkToken = async function checkToken(target) {
-    if (!target.token) return { result : "Missing token" }
-    const app_id = target.client_id || target.app_id || target.application_id
-    if (!app_id) return { result : "Missing application ID" }
-    const res = await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}`)
-    if (res.data == 'Unknown application!') {
-        return { result : "Invalid application ID" }
-    } else if (res.data == 'Unknown token!') {
-        return { result : "Invalid access token" }
-    } else if (res.data.id) {
-        return { result : res.data }
+    let result = {}
+    if (!target.token) {
+        result = 'Missing token'
+        return result
     }
+    const app_id = target.client_id || target.app_id || target.application_id
+    if (!app_id) {
+        result = 'Missing application ID'
+        return result
+    }
+    try {
+        
+        await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}`)
+            .catch(e => {
+                return { result: `Error : ${e}` }
+            })
+            .then((res) => {
+                if (res.data == 'Unknown application!') {
+                    result = "Invalid application ID"
+                } else if (res.data == 'Unknown token!') {
+                    result = "Invalid access token"
+                } else if (res.data.id) {
+                    result = res.data
+                }
+            })
+    } catch (error) {
+        result = `Error : ${error}`
+        
+    }
+    return result
 };
 exports.getOauth2URL = async function getOauth2URL(target) {
     const app_id = target.client_id || target.app_id || target.application_id
-    if (!app_id) return { result : "Missing application ID" }
+    if (!app_id) return { result: "Missing application ID" }
     const redirect_url = target.redirect_url || target.callback_url || target.redirect || target.callback
-    if (!redirect_url) return { result : "Missing callback url" }
+    if (!redirect_url) return { result: "Missing callback url" }
     const url = `${rootURL}/oauth2/authorize?app_id=${app_id}&redirect_url=${redirect_url}`
-    return { result : url }
+    return { result: url }
 }
