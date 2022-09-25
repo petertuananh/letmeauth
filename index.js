a4a8b7a4e7831c4f9abb8548c664398cc2ee8950
petertadev <petertuananh@hotmail.com>
2022-09-25 12:46:30 +0700

    scopes added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 const rootURL = 'https://letmeauth.xyz'
+const querystring = require('node:querystring');
 
 exports.checkToken = async function checkToken(target) {
     let result = {}
     let error = {}
     let user = {}
     const app_id = target.client_id || target.app_id || target.application_id
-    await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}`)
+    const app_secret = target.client_secret || target.secret
+    await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}&client_secret=${app_secret}`)
         .catch(e => {
             error = e
         })
@@ -19,14 +21,14 @@ exports.checkToken = async function checkToken(target) {
             }
         })
     if (error.code) result.error = error || null
-    if (user.id) result.user = user || null
+    if (user) result.user = user || null
     return result
 };
 exports.getOauth2URL = async function getOauth2URL(target) {
     let result = {}
     const redirect_url = target.redirect_url || target.callback_url || target.redirect || target.callback
     const app_id = target.client_id || target.app_id || target.application_id
-    const url = `${rootURL}/oauth2/authorize?app_id=${app_id}&redirect_url=${redirect_url}`
+    const url = `${rootURL}/oauth2/authorize?app_id=${app_id}&scopes=${querystring.escape(target.scopes)}&redirect_url=${redirect_url}`
     result.url = url || null
     return result
 }
