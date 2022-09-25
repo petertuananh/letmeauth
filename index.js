ad133847429adc973b08880b119cbc036af62dc1
petertadev <petertuananh@hotmail.com>
2022-09-25 10:47:01 +0700

    v1.1.2

--- index.js
+++ index.js
@@ -3,41 +3,30 @@ const rootURL = 'https://letmeauth.xyz'
 
 exports.checkToken = async function checkToken(target) {
     let result = {}
-    if (!target.token) {
-        result = 'Missing token'
-        return result
-    }
+    let error = {}
+    let user = {}
     const app_id = target.client_id || target.app_id || target.application_id
-    if (!app_id) {
-        result = 'Missing application ID'
-        return result
-    }
-    try {
-        
-        await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}`)
-            .catch(e => {
-                return { result: `Error : ${e}` }
-            })
-            .then((res) => {
-                if (res.data == 'Unknown application!') {
-                    result = "Invalid application ID"
-                } else if (res.data == 'Unknown token!') {
-                    result = "Invalid access token"
-                } else if (res.data.id) {
-                    result = res.data
-                }
-            })
-    } catch (error) {
-        result = `Error : ${error}`
-        
-    }
+    await axios.default.get(`${rootURL}/oauth2/checktoken?token=${target.token}&app_id=${app_id}`)
+        .catch(e => {
+            error = e
+        })
+        .then((res) => {
+            if (!res) return
+            if (res.data.code) {
+                error = { code: res.data.code, msg: res.data.msg } || null
+            } else if (res.data.id) {
+                user = res.data || null
+            }
+        })
+    if (error.code) result.error = error || null
+    if (user.id) result.user = user || null
     return result
 };
 exports.getOauth2URL = async function getOauth2URL(target) {
-    const app_id = target.client_id || target.app_id || target.application_id
-    if (!app_id) return { result: "Missing application ID" }
+    let result = {}
     const redirect_url = target.redirect_url || target.callback_url || target.redirect || target.callback
-    if (!redirect_url) return { result: "Missing callback url" }
+    const app_id = target.client_id || target.app_id || target.application_id
     const url = `${rootURL}/oauth2/authorize?app_id=${app_id}&redirect_url=${redirect_url}`
-    return { result: url }
+    result.url = url || null
+    return result
 }
